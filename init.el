5f86463f87635075f3c2363b5f91cb2be0b9fb54
shampee <hampouse@gmail.com>
2022-11-05 19:09:55 +0100

    Set the theme to Macish, and changed the font to Go Mono

--- init.el
+++ init.el
@@ -29,10 +29,23 @@
   (package-install 'use-package))
 
 ;; Load Witchmacs theme
-(load-theme 'Witchmacs t)
+(load-theme 'Macish t)
 
 ;; Set font
-(set-frame-font "Px437 Verite 8x16 12" nil t)
+(set-frame-font "Go Mono 9" nil t)
 
 ;; Load config.org for init.el configuration
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
