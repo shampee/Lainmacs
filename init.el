eff58f4d50e6b08c12802efc291d2705a4f17a8c
shampee <hampouse@gmail.com>
2022-12-17 23:51:57 +0100

    Powerline matches with the Macish theme.

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
 (load-theme 'Macish t)
 
 ;; Set font
-(set-frame-font "Go Mono 9" nil t)
+(set-frame-font "Go Mono 10" nil t)
 
 ;; Load config.org for init.el configuration
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
@@ -41,11 +41,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fc6a63def3a8bdbfe0e3f845b5ef87e1a54458b2c76a534148ee4b72019d609a" "a17123ade1598b8e3d7039e35dfe69977b8f7e1a011b0ff899aabf89f0dbb0c5" default))
  '(package-selected-packages
-   '(meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
+   '(slime-company rust-mode company-shell ac-geiser parinfer-rust-mode vertico meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package))
+ '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(powerline-active1 ((t (:inherit mode-line :background "#fg9fg" :foreground "#ef8ef"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "#efe8ef" :foreground "#ef8ef"))))
+ '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "#ef8ef" :foreground "#ef8ef"))))
+ '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "#ef8ef" :foreground "#ef8ef")))))
